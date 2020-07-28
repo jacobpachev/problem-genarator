ebcbae5d78b70a47f8adfe72d18a64d05d83a4f1
jacobpachev <jacobpachev@gmail.com>
2020-07-27 18:49:44 -0600

    Made the code compatible with all borwsers using es6-shim and added a class called function.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-	new Vue({
-		el: '#app',
-		vuetify: new Vuetify(),
-		data: {
-			valid: false,
-			n_problems: 10,
-			n_terms: 3,
-			max_val: 10
+new Vue({
+	el: '#app',
+	vuetify: new Vuetify(),
+	data: {
+		valid: false,
+		n_problems: 10,
+		n_terms: 3,
+		max_val: 10
     },
-		methods: {
-			generate: function () {
-				this.n_problems = parseInt(this.n_problems);
-			}
+	
+	methods: {
+		generate: function () {
+			this.n_problems = parseInt(this.n_problems);
 		}
-	})
+	}
+})
+class fraction {
+	
+	
+};
