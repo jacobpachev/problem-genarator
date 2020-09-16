d6670dc301d4828ba2e668b92dfc7dbbef4a6e96
jacobpachev <jacobpachev@gmail.com>
2020-09-15 19:49:16 -0600

    Added hint file.

---
+++ vue-cli/problem-generator/src/components/Hint.vue
@@ -0,0 +1,12 @@
+<template>
+	<span v-if="op != 'sum_sq' && op != 'table_sq' ">{{this.i.toString() + this.op + this.j.toString()}}</span>
+	<span v-else-if="op == 'sum_sq' ">{{this.i.toString() + " ^ 2 " + "+" + this.j.toString() + " ^ 2 "}}</span>
+	<span v-else-if="op == 'table_sq' && this.i != 0">{{this.i.toString() + this.j.toString() + " ^ 2 "}}</span>
+	<span v-else-if="op == 'table_sq' && this.i == 0">{{this.j.toString() + " ^ 2 "}}</span>
+</template>
+<script>
+export default {
+	name: 'Hint',
+	props: ['root', 'op','i','j'],
+}
+</script>
